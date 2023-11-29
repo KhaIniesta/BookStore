b6979c19a70a1fa005768f32dd00c411b1f9befa
quythuong <quythuong001@gmail.com>
2023-11-29 19:08:30 +0700

    mostly complete Book functionalities

--- Program.cs
+++ Program.cs
@@ -15,8 +15,8 @@ namespace BookStore
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            //Application.Run(new Form_Admin());
-            Application.Run(new Form_Cashier());
+            Application.Run(new Form_Admin());
+            //Application.Run(new Form_Cashier());
             //Application.Run(new Form_SM());
 
         }
