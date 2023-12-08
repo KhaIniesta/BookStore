fa3c686f5fd0c5cf04193782e6f50383c597491c
qdatn <datnq2003@gmail.com>
2023-12-08 17:22:02 +0700

    report revenue

---
+++ Report/Form_RevenueReport.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace BookStore.Report
+{
+    partial class Form_RevenueReport
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cachedreport_ReceiptDetail1 = new BookStore.Report.Cachedreport_ReceiptDetail();
+            this.crystalReportViewer1 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
+            this.rpt_revenue = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
+            this.SuspendLayout();
+            // 
+            // crystalReportViewer1
+            // 
+            this.crystalReportViewer1.ActiveViewIndex = -1;
+            this.crystalReportViewer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.crystalReportViewer1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.crystalReportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.crystalReportViewer1.Location = new System.Drawing.Point(0, 0);
+            this.crystalReportViewer1.Name = "crystalReportViewer1";
+            this.crystalReportViewer1.Size = new System.Drawing.Size(800, 450);
+            this.crystalReportViewer1.TabIndex = 0;
+            // 
+            // rpt_revenue
+            // 
+            this.rpt_revenue.ActiveViewIndex = -1;
+            this.rpt_revenue.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.rpt_revenue.Cursor = System.Windows.Forms.Cursors.Default;
+            this.rpt_revenue.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rpt_revenue.Location = new System.Drawing.Point(0, 0);
+            this.rpt_revenue.Name = "rpt_revenue";
+            this.rpt_revenue.Size = new System.Drawing.Size(800, 450);
+            this.rpt_revenue.TabIndex = 1;
+            // 
+            // Form_RevenueReport
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.rpt_revenue);
+            this.Controls.Add(this.crystalReportViewer1);
+            this.Name = "Form_RevenueReport";
+            this.Text = "Form_RevenueReport";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Cachedreport_ReceiptDetail cachedreport_ReceiptDetail1;
+        private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer1;
+        public CrystalDecisions.Windows.Forms.CrystalReportViewer rpt_revenue;
+    }
+}
--- UserControls/UC_Revenue.cs
+++ UserControls/UC_Revenue.cs
@@ -1,15 +1,9 @@
-﻿using Microsoft.AnalysisServices;
+﻿using BookStore.Report;
+using CrystalDecisions.CrystalReports.Engine;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
 using System.Data.SqlClient;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Windows.Forms.DataVisualization.Charting;
 
 namespace BookStore.UserControls
 {
@@ -22,6 +16,7 @@ namespace BookStore.UserControls
         private DatabaseConnection DBC = new DatabaseConnection();
         SqlConnection conn = new SqlConnection("Data Source=.;Initial Catalog=QLNhaSach;User ID=sa;Password=1234");
         SqlCommand cmd = null;
+        private decimal SumRevenue = 0;
 
         public event EventHandler ChangeUCClicked;
         UC_AllReceiptDetail UC_AllRecDetail;
@@ -69,24 +64,26 @@ namespace BookStore.UserControls
                "TongHD as 'Total'" +
                $"from V_DTNgay where Ngay = {DateTimePicker.Value.Day} and Thang = {DateTimePicker.Value.Month} and Nam = {DateTimePicker.Value.Year}";
             DG_RevenueDetail.DataSource = DBC.GetTable(sql);
+            SumOfRevenue(DG_RevenueDetail, SumRevenue);
         }
 
         private void Btn_Month_Click(object sender, EventArgs e)
         {
             ResetChart();
-                string sql = "select " +
-                "MaHD as 'Receipt ID'," +
-                "Nam as 'Year'," +
-                "Thang as 'Month'," +
-                "Ngay as 'Day'," +
-                "TongHD as 'Total'" +
-                $"from V_DTThang where Thang = {DateTimePicker.Value.Month} and Nam = {DateTimePicker.Value.Year}";
+            string sql = "select " +
+            "MaHD as 'Receipt ID'," +
+            "Nam as 'Year'," +
+            "Thang as 'Month'," +
+            "Ngay as 'Day'," +
+            "TongHD as 'Total'" +
+            $"from V_DTThang where Thang = {DateTimePicker.Value.Month} and Nam = {DateTimePicker.Value.Year}";
             DG_RevenueDetail.DataSource = DBC.GetTable(sql);
             int NumOfDays = 30;
             for (int i = 1; i <= NumOfDays; ++i)
             {
                 Chart_Revenue.Series["Revenue"].Points.AddXY(i, DayRevenue(i, DateTimePicker.Value.Month, DateTimePicker.Value.Year));
             }
+            SumOfRevenue(DG_RevenueDetail, SumRevenue);
         }
 
         private void Btn_Year_Click(object sender, EventArgs e)
@@ -96,16 +93,17 @@ namespace BookStore.UserControls
                 "MaHD as 'Receipt ID'," +
                 "Nam as 'Year'," +
                 "Thang as 'Month'," +
-                "Ngay as 'Day'," + 
+                "Ngay as 'Day'," +
                 "TongHD as 'Total'" +
                 $" from V_DTThang where Nam = {DateTimePicker.Value.Year}";
             DG_RevenueDetail.DataSource = DBC.GetTable(sql);
 
             int NumOfMonths = 12;
-            for(int i = 1; i <= NumOfMonths; ++i)
+            for (int i = 1; i <= NumOfMonths; ++i)
             {
                 Chart_Revenue.Series["Revenue"].Points.AddXY(i, MonthRevenue(i, DateTimePicker.Value.Year));
             }
+            SumOfRevenue(DG_RevenueDetail, SumRevenue);
         }
 
         private void Btn_Details_Click(object sender, EventArgs e)
@@ -125,6 +123,66 @@ namespace BookStore.UserControls
         public void SetUC_AllRecDetail_Reference(UC_AllReceiptDetail UC)
         {
             this.UC_AllRecDetail = UC;
+        }
+
+        private void btn_printRevenue(object sender, EventArgs e)
+        {
+            DataSet ds = new DataSet();
+            DataTable dt = new DataTable();
+
+            foreach (DataGridViewColumn column in DG_RevenueDetail.Columns)
+            {
+                dt.Columns.Add(column.HeaderText, column.ValueType);
+            }
+
+            foreach (DataGridViewRow row in DG_RevenueDetail.Rows)
+            {
+                DataRow dataRow = dt.NewRow();
+                if (row.Cells[0].Value == null)
+                {
+                    break;
+                }
+                else
+                {
+                    for (int i = 0; i < row.Cells.Count; i++)
+                    {
+                        dataRow[i] = row.Cells[i].Value;
+                    }
+                    dt.Rows.Add(dataRow);
+                }
+            }
+
+            ds.Tables.Add(dt);
+            ds.WriteXmlSchema("Revenue.xml");
+
+            // Gán dữ liệu cho report
+            report_Revenue reportRevenue = new report_Revenue();
+            reportRevenue.SetDataSource(dt);
+            // Gán các label vào report
+            TextObject sumofrevenue_obj = (TextObject)reportRevenue.ReportDefinition.Sections["Section3"].ReportObjects["sumofrevenue"];
+            sumofrevenue_obj.Text = txt_sumrevenue.Text;
+
+            // Hiển thị báo cáo
+            Form_RevenueReport f = new Form_RevenueReport();
+            f.rpt_revenue.ReportSource = reportRevenue;
+            f.ShowDialog();
+
+        }
+
+        private void SumOfRevenue(DataGridView dg, decimal sumrevenue)
+        {
+            foreach (DataGridViewRow row in DG_RevenueDetail.Rows)
+            {
+                if (row.Cells[0].Value == null)
+                {
+                    break;
+                }
+                else
+                {
+                    sumrevenue += (decimal)row.Cells[4].Value;
+                }
+            }
+            txt_sumrevenue.Text = sumrevenue.ToString();
         }
     }
 }
